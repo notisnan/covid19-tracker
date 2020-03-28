42c686ed8b27a8fa58e186bec634f46fdcd85a93
Dulio Denis <dulio.denis@yahoo.com>
2020-03-28 01:46:57 -0400

    Dynamically populating top country values to UI

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function toggleForm() {
 // --------------------------
 let countryObj = {};
 let topFiveCountries = [];
+let countriesContainer = document.querySelector('.countries');
 
 fetch('https://thevirustracker.com/free-api?countryTotals=ALL')
 .then((response) => {
@@ -27,11 +28,54 @@ fetch('https://thevirustracker.com/free-api?countryTotals=ALL')
     countryObj[value.title] = value;
   }
   topFiveCountries = topFive(countryObj);
+  // loop over top fiove countries
+  for (let country of topFiveCountries) {
+    // for each country create a row element with all data populated - invoke the createRow(country)
+    // append that row to the countries element
+    countriesContainer.appendChild(createRow(country));
+  }
 });
+
+  // define a function that takes an object 
+  // and in this function [createRow] return an element - that has all the data populated
+  // 1. create a new div
+  // 2. assign a class to that div element > country
+  // 3. set the InnerHTML of this element to the structure of that row 
+  // 4. Inside that structure replace all values from our object
+  // 5. return this element
+function createRow(country) {
+  const div = document.createElement('div');
+  div.classList.add('country');
+  div.innerHTML = `
+    <button class="remove-country">
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 14 14" style="enable-background:new 0 0 14 14;" xml:space="preserve">
+        <path d="M14,1.4L12.6,0L7,5.6L1.4,0L0,1.4L5.6,7L0,12.6L1.4,14L7,8.4l5.6,5.6l1.4-1.4L8.4,7L14,1.4z"/>
+      </svg>
+    </button>
+    <div class="country__name">${country.title}</div>
+    <!-- Confirmed -->
+    <div class="statistic column-confirmed">
+      <div class="statistic__count">${country.total_cases}<span>k</span></div>
+      <div class="statistic__change">+${((country.total_new_cases_today/country.total_cases)*100).toFixed(2)}%</div>
+    </div>
+    <!-- Deaths -->
+    <div class="statistic column-deaths">
+      <div class="statistic__count">${country.total_deaths}<span>k</span></div>
+      <div class="statistic__change">+${((country.total_new_deaths_today/country.total_deaths)*100).toFixed(2)}%</div>
+    </div>
+    <!-- Recovered -->
+    <div class="statistic column-recovered">
+      <div class="statistic__count">${country.total_recovered}<span>k</span></div>
+      <div class="statistic__change"></div>
+    </div>
+  `;
+  return div;
+}
+
 
 // define a function [smallestValue] to find the smallest item 
 // in smallestCountries object
-const getSmallestValue = (countries) => {
+function getSmallestValue(countries) {
   let smallest = countries[0];
   for (let country of countries) {
     if (smallest.total_cases > country.total_cases) smallest = country;
@@ -39,7 +83,7 @@ const getSmallestValue = (countries) => {
   return smallest;
 }
 
-const topFive = (countries) => {    
+function topFive(countries) {    
   // declare an array of objects - smallestCountries
   const sampleCountry = countries["Botswana"];
   const mostCasesArray = [sampleCountry, sampleCountry, sampleCountry, sampleCountry, sampleCountry];
