b3827eb4c2789caa2e1791916759c48a993b510d
Mariusz Dabrowski <wayrse@gmail.com>
2020-03-28 21:13:11 -0400

    Issue #4 - Last digit can round based on cut off numbers value

--- js/main.js
+++ js/main.js
@@ -186,9 +186,9 @@ function createRow(country) {
   return div;
 }
 
-// ---------------------------
-// Calculate percentage change
-// ---------------------------
+// -----------------------------------
+// HELPER: Calculate percentage change
+// -----------------------------------
 
 function calculatePercentage(casesToday, casesTotal) {
   let percent = casesToday/casesTotal;
@@ -204,18 +204,51 @@ function calculatePercentage(casesToday, casesTotal) {
 // ----------------------------
 
 function formatNumber(number) {
+
+  // ---------------
+  // Up to a thousand
+  // ---------------
+
   if (number < 1000) {
     return number;
   }
+
+  // ---------------
+  // Up to a million
+  // ---------------
+
   if (number < 1000000) {
-    const preDigit = String(number).split('');
+    let preDigit = String(number).split('');
+    const removedDigits = Number(preDigit.slice(-2).join(''));
+
     preDigit.splice(-2);
+    
+    // If the removed digits are greater than 50, round last value up
+    if (removedDigits >= 50) {
+      const newValue = Number(preDigit.join('')) + 1;
+      preDigit = String(newValue).split('');
+    }
+
     preDigit.splice(preDigit.length-1, 0, '.');
     preDigit.push('<span>k</span>');
     return preDigit.join('');
   }
-  const preDigit = String(number).split('');
+
+  // ---------------
+  // Up to a billion
+  // ---------------
+
+  let preDigit = String(number).split('');
+  const removedDigits = Number(preDigit.slice(-5).join(''));
+
   preDigit.splice(-5);
+
+  // If the removed digits are greater than 50000, round last value up
+  if (removedDigits >= 50000) {
+    const newValue = Number(preDigit.join('')) + 1;
+    preDigit = String(newValue).split('');
+  }
+
   preDigit.splice(preDigit.length-1, 0, '.');
   preDigit.push('<span>m</span>');
   return preDigit.join('');
