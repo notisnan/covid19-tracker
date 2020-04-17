62a022ad4df386db42ed44c47934708b1eef9f48
Mariusz Dabrowski <wayrse@gmail.com>
2020-04-17 16:42:14 -0400

    removed dot after pop

--- src/components/CountryRow.js
+++ src/components/CountryRow.js
@@ -51,7 +51,7 @@ class CountryRow extends React.Component {
             column__info-top
             ${(country.title.length > 16) ? 'column__info-top--small' : ''}
           `}>{country.title}</div>
-          <div className="column__info-bottom" dangerouslySetInnerHTML={{__html: `pop. ${concatNumber(parseInt(country.cases/country.cases_per_million*1000000))}`}}></div>
+          <div className="column__info-bottom" dangerouslySetInnerHTML={{__html: `pop ${concatNumber(parseInt(country.cases/country.cases_per_million*1000000))}`}}></div>
         </div>
 
         <div className="column column-confirmed">
